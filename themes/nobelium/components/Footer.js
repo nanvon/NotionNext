d62a41f2a247cb67161a9f432b6f460573319972
nanvon <nanvon@foxmail.com>
2023-12-14 13:55:42 +0800

    fix: nobelium使用medium的SocialButton组件

--- themes/nobelium/components/Footer.js
+++ themes/nobelium/components/Footer.js
@@ -1,6 +1,7 @@
 import DarkModeButton from '@/components/DarkModeButton'
-import Vercel from '@/components/Vercel'
+// import Vercel from '@/components/Vercel'
 import { siteConfig } from '@/lib/config'
+import SocialButton from '@/themes/medium/components/SocialButton'
 
 export const Footer = (props) => {
   const d = new Date()
@@ -22,7 +23,8 @@ export const Footer = (props) => {
          <p>
            © {siteConfig('AUTHOR')} {copyrightDate}
          </p>
-         <Vercel />
+        {/* <Vercel /> */}
+        <SocialButton />
        </div>
      </div>
    </footer>
