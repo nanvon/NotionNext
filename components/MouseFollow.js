a95371baafe6c347ba5b33714dade21a3ce02b1c
nanvon <nanvon1984@gmail.com>
2024-05-11 09:10:45 +0800

    revert nest

--- components/MouseFollow.js
+++ components/MouseFollow.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 // import anime from 'animejs'
-// import { siteConfig } from '@/lib/config'
+import { siteConfig } from '@/lib/config'
 import { loadExternalResource } from '@/lib/utils'
 
 /**
@@ -8,19 +8,18 @@ import { loadExternalResource } from '@/lib/utils'
  * @returns
  */
 const MOUSE_FOLLOW = () => {
-  // const type = siteConfig('MOUSE_FOLLOW_EFFECT_TYPE')
-  // const color = siteConfig('MOUSE_FOLLOW_EFFECT_COLOR')
+  const type = siteConfig('MOUSE_FOLLOW_EFFECT_TYPE')
+  const color = siteConfig('MOUSE_FOLLOW_EFFECT_COLOR')
 
   useEffect(() => {
-    // loadExternalResource('/js/mouse-follow.js', 'js').then(url => {
-    //   if (window.createMouseCanvas) {
-    //     window.createMouseCanvas()({
-    //       type,
-    //       color
-    //     })
-    //   }
-    // })
-    loadExternalResource('/js/canvas-nest.js', 'js')
+    loadExternalResource('/js/mouse-follow.js', 'js').then(url => {
+      if (window.createMouseCanvas) {
+        window.createMouseCanvas()({
+          type,
+          color
+        })
+      }
+    })
   }, [])
 
   return <></>
